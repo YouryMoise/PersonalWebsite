8cb1d372d94676ef67ea7a02d7954614e96676a3
Youry Moise <yourym@mit.edu>
2024-08-03 13:22:01 -0700

    Home and SEVT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SevtPageComponent } from './sevt-page/sevt-page.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: 'home', component: HomeComponent},
+  { path: 'sevt', component:SevtPageComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/sevt-page/sevt-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sevt-page',
+  templateUrl: './sevt-page.component.html',
+  styleUrls: ['./sevt-page.component.css']
+})
+export class SevtPageComponent {
+
+}
