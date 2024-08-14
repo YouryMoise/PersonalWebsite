3af4652b05ed36d59d0eda12fb7df2b2500be5ca
Youry Moise <yourym@mit.edu>
2024-08-13 23:10:03 -0700

    Added some projects. Made learn more button expand on hover. Can now add videos to the dark left box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router ) {
+
+  }
   title = 'personal_website';
   currentTab:number = 0;
   changeCurrentTab(x:number){
     this.currentTab = x;
+    // alert(this.router.url)
     // alert(this.currentTab)
   }
+  // This doesn't work because the url is just / by the time this is called
+  // ngOnInit(){
+  //   if (this.router.url == "/extras") this.currentTab = 0;
+  //   if (this.router.url == "/classProjects") this.currentTab = 1;
+  //   if (this.router.url == "/internships") this.currentTab = 2;
+  //   if (this.router.url == "/personal") this.currentTab = 3;
+  // }
 }
